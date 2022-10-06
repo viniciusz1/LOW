1997fffc8d39fe7d47128661c7b335426c5b71a2
VINICIUS BONATTI BENNER <vinicius_benner@estudante.sesisenai.org.br>
2022-10-06 16:27:20 -0300

    cursor:pointer

--- src/app/componentes-reutilizaveis/card-demanda/card-demanda.component.ts
+++ src/app/componentes-reutilizaveis/card-demanda/card-demanda.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbDropdownConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-card-demanda',
   templateUrl: './card-demanda.component.html',
-  styleUrls: ['./card-demanda.component.scss']
+  styleUrls: ['./card-demanda.component.scss'],
+  providers: [NgbDropdownConfig]
 })
 export class CardDemandaComponent implements OnInit {
 
-  constructor() { }
+  constructor(config: NgbDropdownConfig) {
+      config.placement='top-start'
+   }
 
   ngOnInit(): void {
   }

f6b51e7443b9e2ab78c44cca61fb7d6c21d54a3c
Camilly Vitoria <camilly_goltz@estudante.sesisenai.org.br>
2022-11-07 19:11:52 -0300

    Telas Configurações

--- src/app/telas/perfil/perfil.module.ts
+++ src/app/telas/perfil/perfil.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TelaPerfilComponent } from './tela-perfil/tela-perfil.component';
+import { TelaLayoutComponent } from './tela-layout/tela-layout.component';
+import { TelaNotificacoesComponent } from './tela-notificacoes/tela-notificacoes.component';
+import { TelaSugestoesComponent } from './tela-sugestoes/tela-sugestoes.component';
+import { TelaAjudaComponent } from './tela-ajuda/tela-ajuda.component';
 
 
 
 @NgModule({
   declarations: [
-    TelaPerfilComponent
+    TelaPerfilComponent,
+    TelaLayoutComponent,
+    TelaNotificacoesComponent,
+    TelaSugestoesComponent,
+    TelaAjudaComponent
   ],
   imports: [
     CommonModule
---
+++ src/app/telas/perfil/tela-notificacoes/tela-notificacoes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tela-notificacoes',
+  templateUrl: './tela-notificacoes.component.html',
+  styleUrls: ['./tela-notificacoes.component.scss']
+})
+export class TelaNotificacoesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

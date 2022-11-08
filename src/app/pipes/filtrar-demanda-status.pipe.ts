9d5312db4378894818b241875eea7a446b8b68c9
viniciusz1 <vinicius101313@gmail.com>
2022-11-08 19:51:04 -0300

    cores corretas

--- src/app/pipes/filtrar-demanda-status.pipe.ts
+++ src/app/pipes/filtrar-demanda-status.pipe.ts
@@ -8,31 +8,31 @@ import { StatusDemanda } from '../models/statusDemanda.enum';
 export class FiltrarDemandaStatusPipe implements PipeTransform {
 
   transform(demandas: Demanda[], ...titulo: string[]): Demanda[] {
-    if(titulo[0] == "STATUS: Backlog - Classificação"){
+    if(titulo[0] == "Backlog - Classificação"){
       return demandas.filter(d => d.statusDemanda == StatusDemanda.BACKLOG)
     }
-    if(titulo[0] == "Status: Backlog - Propostas"){
+    if(titulo[0] == "Backlog - Propostas"){
       return demandas.filter(d => d.statusDemanda == StatusDemanda.BACKLOG)
     }
-    if(titulo[0] == "Status: Assessment"){
+    if(titulo[0] == "Assessment"){
       return demandas.filter(d => d.statusDemanda == StatusDemanda.ASSESSMENT)
     }
-    if(titulo[0] == "Status: Business Case"){
+    if(titulo[0] == "Business Case"){
       return demandas.filter(d => d.statusDemanda == StatusDemanda.BUSINESS_CASE)
     }
-    if(titulo[0] == "Status: To Do"){
+    if(titulo[0] == "To Do"){
       return demandas.filter(d => d.statusDemanda == StatusDemanda.TO_DO)
     }
-    if(titulo[0] == "Status: Design and Build"){
+    if(titulo[0] == "Design and Build"){
       return demandas.filter(d => d.statusDemanda == StatusDemanda.DESIGN_AND_BUILD)
     }
-    if(titulo[0] == "Status: Support"){
+    if(titulo[0] == "Support"){
       return demandas.filter(d => d.statusDemanda == StatusDemanda.SUPPORT)
     }
-    if(titulo[0] == "Status: Cancelled"){
+    if(titulo[0] == "Cancelled"){
       return demandas.filter(d => d.statusDemanda == StatusDemanda.CANCELLED)
     }
-    if(titulo[0] == "Status: Done"){
+    if(titulo[0] == "Done"){
       return demandas.filter(d => d.statusDemanda == StatusDemanda.DONE)
     }
     return demandas;

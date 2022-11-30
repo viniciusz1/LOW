9af77c6d2726b8e19b411de25fba9a0399f547a3
viniciusz1 <vinicius101313@gmail.com>
2022-11-30 18:34:58 -0300

    tema

--- src/app/telas/perfil/tela-layout/tela-layout.component.ts
+++ src/app/telas/perfil/tela-layout/tela-layout.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tela-layout',
@@ -6,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tela-layout.component.scss']
 })
 export class TelaLayoutComponent implements OnInit {
+  themeSelection: boolean = false
+  constructor(@Inject(DOCUMENT) private document: Document) {
+    let theme = window.localStorage.getItem('theme');
+    if(theme){
+      this.themeSelection = theme == 'dark' ? true : false;
+      this.changeTheme(this.themeSelection);
+    }
+   }
 
-  constructor() { }
-
+  changeTheme(state:boolean){
+    let theme = state ? 'dark' : 'light';
+    window.localStorage.setItem('theme', theme);
+    let themelink = this.document.getElementById('app-theme') as HTMLLinkElement;
+    themelink.href = `bootstrap-${theme}-blue.css`;
+  }
   ngOnInit(): void {
   }
 

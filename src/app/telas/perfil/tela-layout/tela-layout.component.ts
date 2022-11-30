732e1dea78ce2f2add4c5afd8d0ad16a0801a577
viniciusz1 <vinicius101313@gmail.com>
2022-11-30 19:28:11 -0300

    trocando tema da fonte

--- src/app/telas/perfil/tela-layout/tela-layout.component.ts
+++ src/app/telas/perfil/tela-layout/tela-layout.component.ts
@@ -8,22 +8,32 @@ import { Component, Inject, OnInit } from '@angular/core';
 })
 export class TelaLayoutComponent implements OnInit {
   themeSelection: boolean = false
-  constructor(@Inject(DOCUMENT) private document: Document) {
-    let theme = window.localStorage.getItem('theme');
-    if(theme){
-      this.themeSelection = theme == 'dark' ? true : false;
-      this.changeTheme(this.themeSelection);
-    }
+  constructor() {
+    // @Inject(DOCUMENT) private document: Document
+
+    // let theme = window.localStorage.getItem('theme');
+    // if(theme){
+    //   this.themeSelection = theme == 'dark' ? true : false;
+    //   this.changeTheme(this.themeSelection);
+    // }
    }
 
-  changeTheme(state:boolean){
-    let theme = state ? 'dark' : 'light';
-    window.localStorage.setItem('theme', theme);
-    let themelink = this.document.getElementById('app-theme') as HTMLLinkElement;
-    themelink.href = `bootstrap-${theme}-blue.css`;
-  }
+  // changeTheme(state:boolean){
+  //   let theme = state ? 'dark' : 'light';
+  //   window.localStorage.setItem('theme', theme);
+  //   let themelink = this.document.getElementById('app-theme') as HTMLLinkElement;
+  //   themelink.href = `bootstrap-${theme}-blue.css`;
+  // }
 
   fontSize = "13";
+
+  setFontTheme(opc: string){
+    let htmlRoot:HTMLElement = <HTMLElement> document.getElementsByTagName('body')[0];
+    if(htmlRoot != null){
+      htmlRoot.style.fontFamily = opc;
+    }
+  }
+
   setFontSize(opc: string){
    this.fontSize = opc;
 

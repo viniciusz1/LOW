8183d9765a924f9b9afe8f0dd83421ff0cbb10ca
viniciusz1 <vinicius101313@gmail.com>
2022-12-06 21:17:30 -0300

    Arrumando rotas lista

--- src/app/componentes-reutilizaveis/list-demanda/list-demanda.component.ts
+++ src/app/componentes-reutilizaveis/list-demanda/list-demanda.component.ts
@@ -11,12 +11,13 @@ import { StatusDemanda } from 'src/app/models/statusDemanda.enum';
 export class ListDemandaComponent implements OnInit {
   primaryColorClass?: string = "";
   secondaryColorClass?: string = "";
-  
+
   @Output() abrirModal = new EventEmitter();
   @Output() verDocumentoProposta = new EventEmitter<string>();
   @Output() clicouAdicionarPauta = new EventEmitter();
   @Output() modalHistorico = new EventEmitter();
   @Output() irParaChat = new EventEmitter();
+  @Output() abrirModalCriarReuniao = new EventEmitter();
 
   @Input() dadosDemada: Demanda = {};
   @Input() mostrarBotao = true;
@@ -47,10 +48,14 @@ export class ListDemandaComponent implements OnInit {
     else if(this.textoExibidoEmBotaoDependendoRota?.rota == "aprovar"){
       this.verDocumentoProposta.emit('gerente')
     }
+    else if(this.textoExibidoEmBotaoDependendoRota?.rota == 'adicionar a reuniao'){
+      this.abrirModalCriarReuniao.emit()
+    }
     else{
       this.route.navigate([this.textoExibidoEmBotaoDependendoRota?.rota])
     }
   }
+
   exibicaoBotoes(){
     if(this.dadosDemada.statusDemanda == StatusDemanda.BACKLOG_CLASSIFICACAO){
       this.textoExibidoEmBotaoDependendoRota = {rota: "/tela-inicial/classificar-demanda", texto: "Classificar Demanda"}
@@ -62,10 +67,10 @@ export class ListDemandaComponent implements OnInit {
       this.textoExibidoEmBotaoDependendoRota = {rota: "aprovar", texto: "Aprovar Demanda"}
     }
     else if(this.dadosDemada.statusDemanda == StatusDemanda.ASSESSMENT){
-      this.textoExibidoEmBotaoDependendoRota = {rota: "/tela-inicial/nova-pauta", texto: "Adicionar Proposta"}
+      this.textoExibidoEmBotaoDependendoRota = {rota: "adicionar a reuniao", texto: "Adicionar Proposta"}
     }
     else if(this.dadosDemada.statusDemanda == StatusDemanda.BUSINESS_CASE){
-      this.textoExibidoEmBotaoDependendoRota = {rota: "/tela-inicial/nova-pauta", texto: "Adicionar Proposta"}
+      this.textoExibidoEmBotaoDependendoRota = {rota: "adicionar a reuniao", texto: "Adicionar Proposta"}
     }
     else if(this.dadosDemada.statusDemanda == StatusDemanda.CANCELLED){
       this.textoExibidoEmBotaoDependendoRota = {rota: "", texto: "Ver Reprovação"}
@@ -73,6 +78,7 @@ export class ListDemandaComponent implements OnInit {
       this.textoExibidoEmBotaoDependendoRota = {rota: "abre documento", texto: "Ver Mais"}
     }
   }
+
   ngOnInit(): void {
     this.primaryColorClass = this.dadosDemada.statusDemanda;
     this.secondaryColorClass = this.dadosDemada.statusDemanda + "-sec";

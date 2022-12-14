26f7c8fef115f99c271145efd94e179a54a6182c
viniciusz1 <vinicius101313@gmail.com>
2022-12-14 19:57:24 -0300

    fazendo post demanda. ok

--- src/app/telas/demandas/geracao-proposta-ou-demanda/tela-corrida/parte-demanda/parte-demanda.component.ts
+++ src/app/telas/demandas/geracao-proposta-ou-demanda/tela-corrida/parte-demanda/parte-demanda.component.ts
@@ -1,9 +1,8 @@
+import { DemandaService } from './../../../../../services/demanda.service';
 import { CentroCustoService } from './../../../../../services/centro-custo.service';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { CentroCusto } from 'src/app/models/centro-custo.model';
-
-
 
 @Component({
   selector: 'app-parte-demanda',
@@ -11,58 +10,24 @@ import { CentroCusto } from 'src/app/models/centro-custo.model';
   styleUrls: ['./parte-demanda.component.scss'],
 })
 export class ParteDemandaComponent implements OnInit {
-  constructor(private fb: FormBuilder,
-    private centroCustoService: CentroCustoService) {}
-  demandaForm = this.fb.group({
-    tituloDemanda: [''],
-    situacaoAtualDemanda: [''],
-    objetivoDemanda: [''],
-    centroCustoDemanda: [''],
-    beneficioRealDemanda: this.fb.group({
-      moeda: [''],
-      memoriaCalculo: [''],
-      valor: [''],
-    }),
-    beneficioPotencialDemanda: this.fb.group({
-      moeda: [''],
-      memoriaCalculo: [''],
-      valor: [''],
-    }),
-    beneficioQualitativoDemanda: [''],
-    frequenciaDeUsoDemanda: [''],
+  constructor(
+    private fb: FormBuilder,
+    private centroCustoService: CentroCustoService,
+    private demandaService: DemandaService
+  ) {}
 
-    solicitanteDemanda: ['Por localStorage'],
-  });
-
+  demandaForm = this.demandaService.demandaForm;
   selectedCentros: any;
   centrosCusto: CentroCusto[] = [];
   opcoesDeTamanho = [
-    { name: 'Muito Pequena' },
-    { name: 'Pequena' },
-    { name: 'Média' },
-    { name: 'Grande' },
-    { name: 'Muito Grande' },
+     'Muito Pequena' ,
+     'Pequena' ,
+     'Média' ,
+     'Grande' ,
+     'Muito Grande'
   ];
-  opcoesDeMoeda = [
-    { name: 'BRL' },
-    { name: 'EUR' },
-    { name: 'DOL' },
-  ];
-  onSubmitDemanda() {
-    //ARQUIVO -- NÃO FUNCIONA
-    // this.demandaForm.patchValue({
-    //   anexoDemanda: JSON.stringify(this.arquivos),
-    // });
-    // this.demandaService.postDemanda(this.demandaForm.value).subscribe({
-    //     next: (response) => {
-    //       console.log(response)
-    //     },
-    //     error: (err) => {
-    //       console.error(err)
-    //     }
-    //   })
-    // console.log(this.demandaForm.value)
-  }
+  opcoesDeMoeda = [{ name: 'BRL' }, { name: 'EUR' }, { name: 'DOL' }];
+
   ngOnInit(): void {
     this.atualizarCentrosCusto();
   }

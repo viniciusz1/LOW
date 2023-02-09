54ce3b66b228ee2b5630895c4cd8aee736f799c8
viniciusz1 <vinicius101313@gmail.com>
2023-02-09 14:53:40 -0300

    filtro reuniões ok

--- src/app/services/demanda.service.ts
+++ src/app/services/demanda.service.ts
@@ -57,9 +57,9 @@ saveAs(blob, name);
   }
 
   getDemandasFiltradas(filtros: {solicitante: string; codigoDemanda: string; status: string; tamanho: string; tituloDemanda: string; analista: string; departamento: string}){
+
     return this.http.get<Demanda[]>(
-      `http://localhost:8080/demanda/filtro?solicitante=${filtros.solicitante}&codigoDemanda=${filtros.codigoDemanda}&status=${filtros.status}&tamanho=${filtros.tamanho}
-      &tituloDemanda=${filtros.tituloDemanda}&analista=${filtros.analista}&departamento=${filtros.departamento}`
+      `http://localhost:8080/demanda/filtro?solicitante=${filtros.solicitante}&codigoDemanda=${filtros.codigoDemanda}&status=${filtros.status}&tamanho=${filtros.tamanho}&tituloDemanda=${filtros.tituloDemanda}&analista=${filtros.analista}&departamento=${filtros.departamento}`
     );
   }
 

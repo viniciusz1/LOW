03284b11ba7c0a7b0476ff146746086410ed17d9
viniciusz1 <vinicius101313@gmail.com>
2023-02-13 16:42:30 -0300

    notificacoes front

---
+++ src/app/models/notificacao.model.ts
@@ -0,0 +1,10 @@
+export interface Notificacao {
+  codigoNotificacao: number,
+  tituloDemandaNotificacao: string,
+  codigo: number,
+  tipoNotificacao: string,
+  descricaoNotificacao: string,
+  dataNotificacao: Date,
+  horaNotificacao: Date,
+  statusNotificacao: string,
+}

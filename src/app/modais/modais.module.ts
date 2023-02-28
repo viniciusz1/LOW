ed49b19300cb49afea387fc17e6e0e8c3a8c7876
viniciusz1 <vinicius101313@gmail.com>
2023-02-28 13:25:08 -0300

    excluindo modal filtro

--- src/app/modais/modais.module.ts
+++ src/app/modais/modais.module.ts
@@ -6,7 +6,6 @@ import { CommonModule } from '@angular/common';
 import { ModalPropostaDocumentoComponent } from './modal-proposta-documento/modal-proposta-documento.component';
 import { ModalAtaDocumentoComponent } from './modal-ata-documento/modal-ata-documento.component';
 import { ModalParecerComissaoPropostaComponent } from './modal-parecer-comissao-proposta/modal-parecer-comissao-proposta.component';
-import { ModalFiltroDemandasComponent } from './modal-filtro-demandas/modal-filtro-demandas.component';
 import { ModalDemandaDocumentoComponent } from './modal-demanda-documento/modal-demanda-documento.component';
 import { ModalMotivoDevolucaoComponent } from './modal-motivo-devolucao/modal-motivo-devolucao.component';
 import {MatSelectModule} from '@angular/material/select';
@@ -29,7 +28,6 @@ import {CalendarModule} from 'primeng/calendar';
     ModalPropostaDocumentoComponent,
     ModalAtaDocumentoComponent,
     ModalParecerComissaoPropostaComponent,
-    ModalFiltroDemandasComponent,
     ModalDemandaDocumentoComponent,
     ModalMotivoDevolucaoComponent,
     ModalReprovacaoDemandaComponent,

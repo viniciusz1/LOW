f30996aa5383c5a9e6a55edb3d5f4d4acf989225
Vinícius Bonatti Benner <vinicius101313@gmail.com>
2023-03-30 21:37:13 -0300

    autenticando

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -1,11 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { NivelAcesso } from './../models/nivel-acesso.enum';
 import { DemandaAnalista } from 'src/app/models/demanda-analista.model';
 import { Injectable } from '@angular/core';
+import { path } from './path/rota-api';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
+  constructor(private http: HttpClient){
+
+  }
 
   private nivelAcesso: NivelAcesso = NivelAcesso.Analista;
 
@@ -15,5 +20,17 @@ export class UsuarioService {
 
   getRole() {
     return this.nivelAcesso;
+  }
+
+  autenticar(usuario: string, senha: string){
+
+    const config = {
+      withCredentials: true
+  }
+
+    return this.http.post(path + "login/auth", {
+      "usuarioUsuario": usuario,
+      "senhaUsuario": senha
+    }, config)
   }
 }

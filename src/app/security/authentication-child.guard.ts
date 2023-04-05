452700b7597fba724a40cf38b2af5b8ced26fa96
Vinícius Bonatti Benner <vinicius101313@gmail.com>
2023-04-05 13:20:15 -0300

    ok

--- src/app/security/authentication-child.guard.ts
+++ src/app/security/authentication-child.guard.ts
@@ -1,3 +1,4 @@
+import { PermissoesDeRotas } from './permissoes';
 import { path } from './../services/path/rota-api';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
@@ -8,14 +9,15 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class AuthenticationChildGuard implements CanActivateChild {
+  permissoes = PermissoesDeRotas
   constructor(private http: HttpClient) {
 
   }
   canActivateChild(childRoute: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
     this.http.get(path + "login/verify-token").subscribe({
-      next(value) {
-        console.log(value)
-      }, error(err) {
+      next: value => {
+        // this.permissoes.find(val)
+      }, error: err => {
         console.log(err)
       },
     })

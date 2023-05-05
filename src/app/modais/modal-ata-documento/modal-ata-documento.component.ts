e51e6727290f95c62b9093ecca3da0ee13aa6625
Vinícius Bonatti Benner <vinicius101313@gmail.com>
2023-05-05 09:32:49 -0300

    documento ata

--- src/app/modais/modal-ata-documento/modal-ata-documento.component.ts
+++ src/app/modais/modal-ata-documento/modal-ata-documento.component.ts
@@ -1,3 +1,4 @@
+import { path } from './../../services/path/rota-api';
 import { DIALOG_DATA } from '@angular/cdk/dialog';
 import { Component, Inject, OnInit } from '@angular/core';
 import { Reuniao } from 'src/app/models/reuniao.model';
@@ -8,12 +9,12 @@ import { Reuniao } from 'src/app/models/reuniao.model';
   styleUrls: ['./modal-ata-documento.component.scss']
 })
 export class ModalAtaDocumentoComponent implements OnInit {
-
+  path = path;
   constructor(
     @Inject(DIALOG_DATA) public data: { reuniao: Reuniao, tipoAta: string },) {
-      console.log(data)
     this.reuniao = data.reuniao;
     this.tipoAta = data.tipoAta;
+    console.log(this.tipoAta)
   }
 
 

223b67378533e675fa7aa00770e23f86401a0ee4
Otavio Neves <otavio_neves@estudante.sesisenai.org.br>
2023-05-15 21:52:26 -0300

    notificações - ajustes

---
+++ src/app/models/tipoNotificacao.enum.ts
@@ -0,0 +1,10 @@
+export enum TipoNotificacao{
+    CRIOU_DEMANDA = "Criou Demanda",
+    EDITOU_DEMANDA = "Editou Demanda",
+    AVANCOU_STATUS_DEMANDA = "Avançou Status da Demanda",
+    CANCELOU_DEMANDA = "Cancelou Demanda",
+    MARCOU_REUNIAO = "Marcou Reunião",
+    EDITOU_REUNIAO = "Editou Reunião",
+    FINALIZOU_REUNIAO = "Reunião Finalizada",
+    DESMARCOU_REUNIAO = "Desmarcou Reunião"
+}

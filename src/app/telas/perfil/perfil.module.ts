3af4ecea3fff19718de71258d9734bc22fcca29d
Camilly Vitoria <camilly_goltz@estudante.sesisenai.org.br>
2023-06-15 16:04:02 -0300

    Tela Layout, Bug exibição de cards

--- src/app/telas/perfil/perfil.module.ts
+++ src/app/telas/perfil/perfil.module.ts
@@ -7,6 +7,7 @@ import { TelaAjudaComponent } from './tela-ajuda/tela-ajuda.component';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { ColorPickerModule } from 'primeng/colorpicker';
+import { ComponentesReutilizaveisModule } from 'src/app/componentes-reutilizaveis/componentes-reutilizaveis.module';
 
 
 
@@ -22,7 +23,8 @@ import { ColorPickerModule } from 'primeng/colorpicker';
     CommonModule,
     RouterModule,
     SharedModule,
-    ColorPickerModule
+    ColorPickerModule,
+    ComponentesReutilizaveisModule
   ]
 })
 export class PerfilModule { }

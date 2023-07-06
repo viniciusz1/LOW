52dac48cb4ba93e4a0ce9d59c5a77100b575739d
Vinícius Bonatti Benner <vinicius101313@gmail.com>
2023-07-05 22:52:54 -0300

    sidebar cores dinâmicas

--- src/app/componentes-reutilizaveis/sidebar-reuniao/sidebar-reuniao.component.ts
+++ src/app/componentes-reutilizaveis/sidebar-reuniao/sidebar-reuniao.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Personalizacao } from 'src/app/models/personalizacao.model';
 
 @Component({
   selector: 'app-sidebar-reuniao',
@@ -10,10 +11,18 @@ export class SidebarReuniaoComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    let personalizacao = localStorage.getItem('personalizacao');
+    let persona: Personalizacao | undefined;
+    if(personalizacao){
+      persona = JSON.parse(personalizacao);
+    }
+    this.listCoresPrimarias = persona?.coresPrimariasReuniaoPersonalizacao;
+    console.log(this.listCoresPrimarias)
   }
   isCollapsed: boolean = false;
   @Output() clicouNaSeta = new EventEmitter();
   @Output() clicouEmUmStatus = new EventEmitter();
+  listCoresPrimarias: any;
 
   onClick() {
     this.isCollapsed = !this.isCollapsed;

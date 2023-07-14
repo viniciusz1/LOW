9b39e57765696051c265884efac90ec091da6e4a
Otavio Neves <otavio_neves@estudante.sesisenai.org.br>
2023-07-14 16:53:36 -0300

    layout - tela configurações

--- src/app/telas/perfil/tela-config-inicial/tela-config-inicial.component.ts
+++ src/app/telas/perfil/tela-config-inicial/tela-config-inicial.component.ts
@@ -17,6 +17,7 @@ export class TelaConfigInicialComponent implements OnInit {
   ngOnInit(): void {
     let local = localStorage.getItem('ordemExibicaoDemandasAtivada')
     let local2 = localStorage.getItem('ordemExibicaoDemandasDesativada')
+    let local3 = localStorage.getItem('personalizacao')
     if (local) {
       this.statusOrdemAtivada = JSON.parse(local)
     }else{
@@ -26,10 +27,23 @@ export class TelaConfigInicialComponent implements OnInit {
     if (local2) {
       this.statusOrdemDesativada = JSON.parse(local2)
     }
+
+    
+    if(local3){
+    let listaCores = JSON.parse(local3).coresPrimariasPersonalizacao;
+    // Troca das cores de Assessment e Business Case
+    let ass = listaCores[5];
+    listaCores[5] = listaCores[4];
+    listaCores[4] = ass;
+
+    this.padrao.forEach((item: any, index: any) => {
+      item.cor = `0.5vw solid ${listaCores[index]}`;
+    });
+    }
   }
   statusOrdemAtivada: any = [];
   statusOrdemDesativada: any = [];
-  padrao: any =[{"status":"SUAS_DEMANDAS","titulo":"Suas Demandas"},{"status":"BACKLOG_CLASSIFICACAO","titulo":"Backlog - Classificação"},{"status":"BACKLOG_APROVACAO","titulo":"Backlog - Aprovação"},{"status":"BACKLOG_PROPOSTA","titulo":"Backlog - Propostas"},{"status":"BUSINESS_CASE","titulo":"Business Case"},{"status":"ASSESSMENT","titulo":"Assessment"},{"status":"DISCUSSION","titulo":"Discussion"},{"status":"TO_DO","titulo":"To Do"},{"status":"DESIGN_AND_BUILD","titulo":"Design and Build"},{"status":"SUPPORT","titulo":"Support"},{"status":"CANCELLED","titulo":"Cancelled"},{"status":"DONE","titulo":"Done"}]
+  padrao: any =[{"status":"SUAS_DEMANDAS","titulo":"Suas Demandas", "cor": "#72bbf7"},{"status":"BACKLOG_CLASSIFICACAO","titulo":"Backlog - Classificação", "cor": "#00579D"},{"status":"BACKLOG_APROVACAO","titulo":"Backlog - Aprovação", "cor": "#00579D"},{"status":"BACKLOG_PROPOSTA","titulo":"Backlog - Propostas", "cor": "#00579D"},{"status":"BUSINESS_CASE","titulo":"Business Case", "cor": "#FFDD43"},{"status":"ASSESSMENT","titulo":"Assessment", "cor": "#8862A2"},{"status":"DISCUSSION","titulo":"Discussion", "cor": "#b4b0a8"},{"status":"TO_DO","titulo":"To Do", "cor": "#EF8300"},{"status":"DESIGN_AND_BUILD","titulo":"Design and Build", "cor": "#000000"},{"status":"SUPPORT","titulo":"Support", "cor": "#0091BD"},{"status":"CANCELLED","titulo":"Cancelled", "cor": "#EA1010"},{"status":"DONE","titulo":"Done", "cor": "#82A584"}]
 
   salvarAlteracoes() {
     localStorage.setItem('ordemExibicaoDemandasAtivada', JSON.stringify(this.statusOrdemAtivada))

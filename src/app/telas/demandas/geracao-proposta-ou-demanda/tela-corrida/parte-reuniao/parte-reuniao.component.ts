498e83d18ac27b0d49a7fca93ed6a13083c32807
Camilly Vitoria <camilly_goltz@estudante.sesisenai.org.br>
2023-07-14 15:59:46 -0300

    Responsividade, bug criação proposta

--- src/app/telas/demandas/geracao-proposta-ou-demanda/tela-corrida/parte-reuniao/parte-reuniao.component.ts
+++ src/app/telas/demandas/geracao-proposta-ou-demanda/tela-corrida/parte-reuniao/parte-reuniao.component.ts
@@ -104,8 +104,10 @@ export class ParteReuniaoComponent implements OnInit {
   perfilDaDespesa = [{ tipo: 'Hardware', value: 'hardware' }, { tipo: 'Software', value: 'software' }, { tipo: 'Corporativo', value: 'corporativo' }];
   valorHoraRecursoValue: number | undefined;
   qtdHorasRecurso: number | undefined;
+  porcentagem: number | null = null;  
   periodoExecucao: number | undefined;
   centroCustoValor: number | undefined;
+  codigoPPM: number | undefined;
 
 
 
@@ -124,8 +126,14 @@ export class ParteReuniaoComponent implements OnInit {
   //fazer verificações necessárias
   addRowRecurso() {
     try {
+      console.log(this.formRecursos.controls.centroCustoRecurso.controls);
       this.propostaService.addRowRecurso()
       this.mudarCustoTotalProjetoEPayback()
+      if(this.formRecursos.controls.centroCustoRecurso.controls.length > 1){
+        for(let i = this.formRecursos.controls.centroCustoRecurso.controls.length; i >= 1; i--){
+          this.removerCentroDeCusto(i)
+        }
+      }
     } catch (err) {
       this.showError("Não foi possível adicionar recurso")
     }
